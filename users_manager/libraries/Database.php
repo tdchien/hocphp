65e933f829ef89c253ef3ac4a65d4e433df2ecb5
chien Tran <br.tdchien@gmail.com>
2015-06-18 15:38:08 +0700

    convert to tab

--- users_manager/libraries/Database.php
+++ users_manager/libraries/Database.php
@@ -1,37 +1,37 @@
 <?php
 class Database {
-    protected $dbHost = 'localhost';
-    protected $dbName = 'hocphp';
-    protected $dbUser = 'hocphp';
-    protected $dbPass = 'hocphp';
-    public $conn = NULL;
-    protected $result = NULL;
+	protected $dbHost = 'localhost';
+	protected $dbName = 'hocphp';
+	protected $dbUser = 'hocphp';
+	protected $dbPass = 'hocphp';
+	public $conn = NULL;
+	protected $result = NULL;
 
-    public function __construct(){
-        $this->conn = mysql_connect($this->dbHost, $this->dbUser, $this->dbPass);
-        if ($this->conn) {
-            $dbSelect = mysql_select_db($this->dbName, $this->conn);
-                if ($dbSelect) {
-                    mysql_query("SET NAMES 'utf8'");
-                } else{
-                    die('Không tìm thấy cơ sở dữ liệu');
-                }
-        } else{
-            die('Không kết nối được tới CSDL');
-        }
-    }
+	public function __construct(){
+		$this->conn = mysql_connect($this->dbHost, $this->dbUser, $this->dbPass);
+		if ($this->conn) {
+			$dbSelect = mysql_select_db($this->dbName, $this->conn);
+				if ($dbSelect) {
+					mysql_query("SET NAMES 'utf8'");
+				} else{
+					die('Không tìm thấy cơ sở dữ liệu');
+				}
+		} else{
+			die('Không kết nối được tới CSDL');
+		}
+	}
 
-    public function query($sql){
-        $result = mysql_query($sql, $this->conn);
-        $data = array();
-        while ($item = mysql_fetch_array($result)) {
-            $data[] = $item;
-        }
-        return $data;
-    }
+	public function query($sql){
+		$result = mysql_query($sql, $this->conn);
+		$data = array();
+		while ($item = mysql_fetch_array($result)) {
+			$data[] = $item;
+		}
+		return $data;
+	}
 
-    public function execute($sql){
-        $result = mysql_query($sql, $this->conn);
-        return $result;
-    }
+	public function execute($sql){
+		$result = mysql_query($sql, $this->conn);
+		return $result;
+	}
 }
--- users_manager/models/user_model.php
+++ users_manager/models/user_model.php
@@ -2,44 +2,44 @@
 include_once('base_model.php');
 
 class User_model {
-    protected $userid;
-    protected $fullname;
-    protected $username;
-    protected $password;
-    protected $email;
-    protected $db;
+	protected $userid;
+	protected $fullname;
+	protected $username;
+	protected $password;
+	protected $email;
+	protected $db;
 
-    public function __construct() {
-        global $db;
-        $this->db = $db;
-    }
+	public function __construct() {
+		global $db;
+		$this->db = $db;
+	}
 
-    public function getOne($user_id = false) {
-        $result = $this->db->query("SELECT * FROM users WHERE userid = {$user_id}");
-        return isset($result[0]) ? $result[0] : false;
-    }
+	public function getOne($user_id = false) {
+		$result = $this->db->query("SELECT * FROM users WHERE userid = {$user_id}");
+		return isset($result[0]) ? $result[0] : false;
+	}
 
-    public function getAll() {
-        return $this->db->query('SELECT * FROM users');
-    }
+	public function getAll() {
+		return $this->db->query('SELECT * FROM users');
+	}
 
-    public function saveOne($data, $id) {
-        $sql = 'UPDATE users SET ';
-        foreach ($data as $key => $value) {
-            $sql .= "`{$key}` = '{$value}',";
-        }
-        $sql = trim($sql, ',');
-        $sql .= " WHERE userid = $id";
-        return $this->db->execute($sql);
-    }
+	public function saveOne($data, $id) {
+		$sql = 'UPDATE users SET ';
+		foreach ($data as $key => $value) {
+			$sql .= "`{$key}` = '{$value}',";
+		}
+		$sql = trim($sql, ',');
+		$sql .= " WHERE userid = $id";
+		return $this->db->execute($sql);
+	}
 
-    public function deleteOne($user_id) {
-        $data = $this->db->execute("DELETE FROM users WHERE userid = '$user_id'");
-        return (count($data) > 0) ? true : false;
-    }
+	public function deleteOne($user_id) {
+		$data = $this->db->execute("DELETE FROM users WHERE userid = '$user_id'");
+		return (count($data) > 0) ? true : false;
+	}
 
-    public function login($username, $password){
-        $data = $this->db->query("SELECT * FROM users WHERE username = '$username' AND password = '$password'");
-        return (count($data) > 0) ? true : false;
-    }
+	public function login($username, $password){
+		$data = $this->db->query("SELECT * FROM users WHERE username = '$username' AND password = '$password'");
+		return (count($data) > 0) ? true : false;
+	}
 }
